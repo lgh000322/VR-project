150b89c8b3dede1e081f22ddcb073bcb739fc548
lgh000322 <kang000322@gmail.com>
2024-05-27 15:12:34 +0900

    씬넘어갈때 텍스트데이터도 같이 넘어가는 기능 구현

---
+++ Assets/Script/GameManagement/SetBallText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetBallText : MonoBehaviour
+{
+    public Transform ballParent; // 공들의 부모 오브젝트
+
+    void Start()
+    {
+        int textCount = PlayerPrefs.GetInt("TextCount", 0);
+
+        // 부모 오브젝트 아래에 있는 모든 공을 배열로 가져오기
+        GameObject[] ballPlayers = new GameObject[ballParent.childCount];
+        for (int i = 0; i < ballParent.childCount; i++)
+        {
+            ballPlayers[i] = ballParent.GetChild(i).gameObject;
+        }
+
+        for (int i = 0; i < textCount; i++)
+        {
+            string passedText = PlayerPrefs.GetString("PassedText" + i, "기본값");
+
+            // 각 공에 텍스트 할당
+            if (i < ballPlayers.Length)
+            {
+                Text ballTextComponent = ballPlayers[i].GetComponentInChildren<Text>();
+                if (ballTextComponent != null)
+                {
+                    ballTextComponent.text = passedText;
+                }
+            }
+        }
+    }
+}
--- Assets/Script/SceneManagement/StartGame.cs
+++ Assets/Script/SceneManagement/StartGame.cs
@@ -1,12 +1,45 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class StartGame : MonoBehaviour
 {
-       public void LoadGameScene()
+    public InputField[] inputFields;
+
+    public void LoadGameScene()
     {
-        Debug.Log("게임 시작 버튼이 눌렸습니다.");
+        string[] textsToPass = new string[8];
+        for (int i = 0; i < 8; i++)
+        {
+            textsToPass[i] = inputFields[i].text;
+        }
+
+        // 텍스트를 섞기
+        textsToPass = ShuffleArray(textsToPass);
+
+        // PlayerPrefs에 텍스트 저장
+        for (int i = 0; i < textsToPass.Length; i++)
+        {
+            PlayerPrefs.SetString("PassedText" + i, textsToPass[i]);
+            Debug.Log(textsToPass[i]);
+        }
+
+        PlayerPrefs.SetInt("TextCount", textsToPass.Length);
         SceneManager.LoadScene("Main");
+    }
+
+    private string[] ShuffleArray(string[] array)
+    {
+        System.Random rand = new System.Random();
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = rand.Next(0, i + 1);
+            string temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
     }
 }
